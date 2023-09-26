5d87a4bd21e54fa1bb9a59c5c87269b1966addd4
Jalal-Assaly <140311958+Jalal-Assaly@users.noreply.github.com>
2023-09-26 16:02:48 +0300

    Delete task13-book-store-spring-webapp directory

--- task13-book-store-spring-webapp/src/main/java/com/example/bookstore/services/extra/UnsuitableBookException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstore.services.extra;
-
-public class UnsuitableBookException extends RuntimeException {
-    public UnsuitableBookException(String message) {
-        super(message);
-    }
-}
